bfd7b857d54ab73c383d8c470065d5ed94877a8b
一闪一闪亮晶晶 <resetsix223@gmail.com>
2023-11-14 12:49:36 +0800

    feat: 搭建基础环境

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { create, Workbench } from "@dtinsight/molecule";
 import "@dtinsight/molecule/esm/style/mo.css";
 
 const moInstance = create({
-  extensions: [],
+	extensions: [],
 });
 
 const App = () => moInstance.render(<Workbench />);
